52edcd350c69ebf5f10fe90b541cd75586fc5b33
FahrulNa <140799150+FahrulNa@users.noreply.github.com>
2023-10-04 14:49:28 +0700

    Add files via upload

---
+++ login_reg_function.js
@@ -0,0 +1,9 @@
+function login(username,pass) {
+  console.log("-----------------------------------")
+  console.log("login")
+  console.log("-----------------------------------")
+  console.log("username:"+username)
+  console.log("password:"+pass)
+}
+
+login("fahrul","****************")
